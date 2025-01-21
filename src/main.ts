de2ad28296c2f4d5124e15c5396f8a5eead79ae4
Kelvin Davila <kelvin.davila@positivo.com.br>
2025-01-21 00:01:16 -0300

    feat: config CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Permite todas as origens (não recomendado em prd, nesse caso configure os clients para o CORS)
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
